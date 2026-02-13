ac2f49889fc07c3cc2d972722a8924b26d86746f
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2026-02-12 21:14:02 -0500

    wip

--- include/tgn.h
+++ include/tgn.h
@@ -76,6 +76,39 @@ auto scatter_max(torch::Tensor src, torch::Tensor index, int dim_size)
                                                  /* include_self*/ false);
 }
 
+auto scatter_add(torch::Tensor src, torch::Tensor index, int dim_size)
+    -> torch::Tensor {
+  return src.new_zeros(dim_size).scatter_reduce_(/*dim*/ 0, index, src,
+                                                 /* reduce */ "sum",
+                                                 /* include_self*/ false);
+}
+
+auto scatter_softmax(torch::Tensor src, torch::Tensor index, int num_nodes)
+    -> torch::Tensor {
+  auto src_max = scatter_max(src.detach(), index, /* dim_size*/ num_nodes);
+  auto out = src - src_max.index_select(0, index);
+  out = out.exp();
+
+  auto out_sum = scatter_max(out, index, /*dim_size*/ num_nodes) + 1e-16;
+  out_sum = out_sum.index_select(0, index);
+  return out / out_sum;
+}
+
+auto scatter_argmax(torch::Tensor src, torch::Tensor index, int dim_size)
+    -> torch::Tensor {
+  auto res = src.new_empty(dim_size);
+  res.scatter_reduce_(0, index, src.detach(), /* reduction*/ "amax",
+                      /* include_self */ false);
+  auto out = index.new_full({dim_size}, /*fill_value*/ dim_size - 1);
+
+  auto mask = src == res.index_select(0, index);
+  auto nonzero = torch::nonzero(mask).view(-1);
+  auto target_indices = index.index_select(0, nonzero);
+  out.index_put_({target_indices}, nonzero);
+
+  return out;
+}
+
 struct TransformerConvImpl : torch::nn::Module {
   TransformerConvImpl(std::size_t in_channels, std::size_t out_channels,
                       std::size_t edge_dim, std::size_t heads,
@@ -117,22 +150,10 @@ struct TransformerConvImpl : torch::nn::Module {
     alpha = torch::dropout(alpha, dropout, is_training());
 
     auto out = (v.index_select(0, j) + e) * alpha.view({-1, heads, 1});
-    // TODO(kuba): Scatter add
-    // out = aggr_module(out, i, dim=0, dim_size=w_q->weight.size(0));
+    out = scatter_add(out, /* index */ i, /*dim_size*/ w_q->weight.size(0));
 
     out = out.view({num_nodes, heads * out_channels});
     return out + w_skip->forward(x);
-  }
-
-  auto scatter_softmax(torch::Tensor src, torch::Tensor index, int num_nodes)
-      -> torch::Tensor {
-    auto src_max = scatter_max(src.detach(), index, /* dim_size*/ num_nodes);
-    auto out = src - src_max.index_select(0, index);
-    out = out.exp();
-
-    auto out_sum = scatter_max(out, index, /*dim_size*/ num_nodes) + 1e-16;
-    out_sum = out_sum.index_select(0, index);
-    return out / out_sum;
   }
 
   torch::nn::Linear w_k{nullptr}, w_q{nullptr}, w_v{nullptr}, w_e{nullptr},
@@ -432,21 +453,6 @@ struct TGNMemoryImpl : torch::nn::Module {
       auto mask = argmax < msg.size(0);  // Filter items with at least one entry
       out.index_put_({mask}, argmax.index({mask}));
     }
-
-    return out;
-  }
-
-  auto scatter_argmax(torch::Tensor src, torch::Tensor index, int dim_size)
-      -> torch::Tensor {
-    auto res = src.new_empty(dim_size);
-    res.scatter_reduce_(0, index, src.detach(), /* reduction*/ "amax",
-                        /* include_self */ false);
-    auto out = index.new_full({dim_size}, /*fill_value*/ dim_size - 1);
-
-    auto mask = src == res.index_select(0, index);
-    auto nonzero = torch::nonzero(mask).view(-1);
-    auto target_indices = index.index_select(0, nonzero);
-    out.index_put_({target_indices}, nonzero);
 
     return out;
   }
