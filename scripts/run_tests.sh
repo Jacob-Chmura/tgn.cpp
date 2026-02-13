b9fa066891db48594b6ba72e4da494527458c2fc
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2026-02-13 08:44:28 -0500

    wip

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -6,24 +6,15 @@ PROJECT_ROOT="$SCRIPT_DIR/.."
 BUILD_DIR="$PROJECT_ROOT/build"
 BUILD_TYPE="Debug"
 
-if [[ $# -ge 1 ]]; then
-  BUILD_TYPE="$1"
-fi
-
-case "$BUILD_TYPE" in
-  Debug|Release|RelWithDebInfo|MinSizeRel) ;;
-  *)
-    echo "Invalid build type: $BUILD_TYPE, try 'Debug|Release|RelWithDebInfo|MinSizeRel'"
-    exit 1
-    ;;
-esac
-
 echo "Configuring CMake project (Build type: $BUILD_TYPE) in $BUILD_DIR"
 mkdir -p "$BUILD_DIR" && cd "$BUILD_DIR"
 
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+      -DTGN_BUILD_TESTS=ON \
       -DCMAKE_PREFIX_PATH=/home/kuba/repo/tgn.cpp/libtorch/ \
       -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" \
       ..
 
 cmake --build . --parallel "$(nproc)"
+
+ctest --output-on-failure
