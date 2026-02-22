07a322184cfe2a3729ab4d9d7035b5004fed1508
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2026-02-22 13:57:36 -0500

    upd

--- scripts/download_tgb_data.sh
+++ scripts/download_tgb_data.sh
@@ -10,6 +10,11 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
+if ! command -v uv >/dev/null 2>&1; then
+    echo "uv is not installed. Install it first: https://docs.astral.sh/uv/getting-started/installation/"
+    exit 1
+fi
+
 DATASET_NAME="$1"
 DEST_DATA_DIR="$PROJECT_ROOT/data/$DATASET_NAME"
 
@@ -18,29 +23,47 @@ mkdir -p "$DEST_DATA_DIR"
 uv run --no-project --with py-tgb --with numpy python - <<EOF
 import numpy as np
 from tgb.linkproppred.dataset import LinkPropPredDataset
+from tgb.linkproppred.negative_sampler import NegativeEdgeSampler
 from tgb.nodeproppred.dataset import NodePropPredDataset
+from tgb.utils.info import DATA_VERSION_DICT, PROJ_DIR
 
 name, dest = "$DATASET_NAME", "$DEST_DATA_DIR"
 print(f"Loading TGB dataset: {name}")
 
+is_link = name.startswith('tgbl-')
 ds = NodePropPredDataset(name=name) if name.startswith('tgbn-') else LinkPropPredDataset(name=name)
 data = ds.full_data
 
-masks = {
-    'train': ds.train_mask,
-    'val': ds.val_mask,
-    'test': ds.test_mask
-}
+masks = {'train': ds.train_mask, 'val': ds.val_mask, 'test': ds.test_mask}
+ns = NegativeEdgeSampler(dataset_name=name) if is_link else None
 
 for split, mask in masks.items():
-    data_cols = [data['sources'][mask], data['destinations'][mask], data['timestamps'][mask]]
+    s_src, s_dst, s_time = data['sources'][mask], data['destinations'][mask], data['timestamps'][mask]
+    cols = [s_src, s_dst, s_time]
     header = "src,dst,time"
 
     if data['edge_feat'] is not None:
-        data_cols.append(data['edge_feat'][mask])
+        cols.append(data['edge_feat'][mask])
         header += "," + ",".join(f"msg_{i}" for i in range(data['edge_feat'].shape[1]))
 
+    if ns and split in ['val', 'test']:
+        print(f"Extracting negatives for {split}")
+
+        v_suffix = f'_v{DATA_VERSION_DICT[name]}' if DATA_VERSION_DICT.get(name, 1) > 1 else ''
+        ns_path = f"{PROJ_DIR}datasets/{name.replace('-', '_')}/{name}_{split}_ns{v_suffix}.pkl"
+        ns.load_eval_set(fname=ns_path, split_mode=split)
+
+        # TODO(kuba) We only work with homogenous number of negatives per positive, for now
+        negs = ns.query_batch(s_src, s_dst, s_time, split_mode=split)
+        n_neg = min(len(x) for x in negs)
+        neg_array = np.array([x[:n_neg] for x in negs], dtype=np.int32)
+
+        cols.append(neg_array)
+        header += "," + ",".join(f"neg_{i}" for i in range(n_neg))
+
     out_path = f"{dest}/{split}.csv"
-    np.savetxt(out_path, np.column_stack(data_cols), delimiter=",", header=header, comments='', fmt='%g')
-    print(f"  - Saved {split} split ({len(data_cols[0])} edges) to {out_path}")
+    np.savetxt(out_path, np.column_stack(cols), delimiter=",", header=header, comments='', fmt='%g')
+    print(f"Saved {split} split ({len(s_src)} edges) to {out_path}")
+
+print("Done.")
 EOF
