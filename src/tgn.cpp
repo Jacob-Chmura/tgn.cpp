59f59c6e1ae46b146d4272634cada75f60a0208d
Jacob Chmura <jacobpaul.chmura@gmail.com>
2026-03-07 11:10:19 -0500

    Vectorize MsgStore (#33)
    * wip
    
    * wip
    
    * WIP

--- src/tgn.cpp
+++ src/tgn.cpp
@@ -118,6 +118,40 @@ struct TransformerConvImpl : torch::nn::Module {
 TORCH_MODULE(TransformerConv);
 
 struct TGNMemoryImpl : torch::nn::Module {
+  struct MsgStore {
+    torch::Tensor src_, dst_, time_, msg_;
+
+    MsgStore(std::int64_t num_nodes, std::int64_t msg_dim) {
+      src_ = torch::zeros({num_nodes}, torch::kLong);
+      dst_ = torch::zeros({num_nodes}, torch::kLong);
+      time_ = torch::zeros({num_nodes}, torch::kLong);
+      msg_ = torch::zeros({num_nodes, msg_dim}, torch::kFloat);
+    }
+
+    auto reset() -> void {
+      src_.zero_();
+      dst_.zero_();
+      time_.zero_();
+      msg_.zero_();
+    }
+
+    auto update(const torch::Tensor& src, const torch::Tensor& dst,
+                const torch::Tensor& time, const torch::Tensor msg) -> void {
+      // Find the index of the last (max time) interaction for each source node
+      auto argmax = scatter_argmax(time, src, src_.size(0));
+
+      // mask out nodes that didn't appear in this batch
+      auto mask = argmax < src.size(0);
+      auto active_node_ids = torch::nonzero(mask).view(-1);
+      auto batch_indices = argmax.index({mask});
+
+      src_.index_put_({active_node_ids}, src.index_select(0, batch_indices));
+      dst_.index_put_({active_node_ids}, dst.index_select(0, batch_indices));
+      time_.index_put_({active_node_ids}, time.index_select(0, batch_indices));
+      msg_.index_put_({active_node_ids}, msg.index_select(0, batch_indices));
+    }
+  };
+
   explicit TGNMemoryImpl(const TGNConfig& cfg, const TimeEncoder& time_encoder,
                          std::int64_t msg_dim, std::int64_t num_nodes)
       : msg_dim_(msg_dim),
@@ -128,7 +162,9 @@ struct TGNMemoryImpl : torch::nn::Module {
                                   torch::TensorOptions().dtype(torch::kLong))),
         assoc_(torch::empty({num_nodes},
                             torch::TensorOptions().dtype(torch::kLong))),
-        time_encoder_(time_encoder) {
+        time_encoder_(time_encoder),
+        src_store_(num_nodes, msg_dim),
+        dst_store_(num_nodes, msg_dim) {
     register_buffer("memory_", memory_);
     register_buffer("last_update_", last_update_);
     register_buffer("assoc_", assoc_);
@@ -139,12 +175,16 @@ struct TGNMemoryImpl : torch::nn::Module {
     gru_ =
         register_module("gru_", torch::nn::GRUCell(cell_dim, cfg.memory_dim));
 
-    src_store_.resize(num_nodes_);
-    dst_store_.resize(num_nodes_);
     reset_state();
 
-    const auto bytes =
-        memory_.nbytes() + last_update_.nbytes() + assoc_.nbytes();
+    auto get_store_bytes = [](const MsgStore& s) {
+      return s.src_.nbytes() + s.dst_.nbytes() + s.time_.nbytes() +
+             s.msg_.nbytes();
+    };
+
+    const auto bytes = memory_.nbytes() + last_update_.nbytes() +
+                       assoc_.nbytes() + get_store_bytes(src_store_) +
+                       get_store_bytes(dst_store_);
     TGN_LOG_INFO(
         "TGNMemory: ~{:.2f} MiB allocated ({} nodes, memory_dim: {}, msg_dim: "
         "{}, gru_cell_dim: {})",
@@ -156,7 +196,8 @@ struct TGNMemoryImpl : torch::nn::Module {
     TGN_LOG_DEBUG("TGNMemory: Resetting state");
     memory_.zero_();
     last_update_.zero_();
-    reset_msg_store();
+    src_store_.reset();
+    dst_store_.reset();
   }
 
   auto detach() -> void { memory_.detach_(); }
@@ -175,11 +216,11 @@ struct TGNMemoryImpl : torch::nn::Module {
 
     if (is_training()) {
       update_memory(n_id);
-      update_msg_store(src, dst, t, raw_msg, true);
-      update_msg_store(dst, src, t, raw_msg, false);
+      src_store_.update(src, dst, t, raw_msg);
+      dst_store_.update(dst, src, t, raw_msg);
     } else {
-      update_msg_store(src, dst, t, raw_msg, true);
-      update_msg_store(dst, src, t, raw_msg, false);
+      src_store_.update(src, dst, t, raw_msg);
+      dst_store_.update(dst, src, t, raw_msg);
       update_memory(n_id);
     }
   }
@@ -191,22 +232,13 @@ struct TGNMemoryImpl : torch::nn::Module {
           "TGNMemory: Switching to Eval. Flushing memory for all {} nodes",
           num_nodes_);
       update_memory(torch::arange(static_cast<std::int64_t>(num_nodes_)));
-      reset_msg_store();
+      src_store_.reset();
+      dst_store_.reset();
     }
     torch::nn::Module::train(mode);
   }
 
  private:
-  auto reset_msg_store() -> void {
-    // Message store format: (src, dst, t, msg)
-    const auto i = torch::empty(0, torch::TensorOptions().dtype(torch::kLong));
-    const auto msg = torch::empty({0, static_cast<std::int64_t>(msg_dim_)});
-    const auto empty_entry = std::make_tuple(i, i, i, msg);
-
-    std::ranges::fill(src_store_, empty_entry);
-    std::ranges::fill(dst_store_, empty_entry);
-  }
-
   auto update_memory(const torch::Tensor& n_id) -> void {
     auto [memory_nid, last_update_nid] = get_updated_memory(n_id);
     memory_.index_put_({n_id}, memory_nid);
@@ -226,6 +258,23 @@ struct TGNMemoryImpl : torch::nn::Module {
     const auto msg = torch::cat({msg_s, msg_d}, 0);
     const auto t = torch::cat({t_s, t_d}, 0);
 
+    auto last_aggr = [&](const torch::Tensor& _msg, const torch::Tensor& _index,
+                         const torch::Tensor& _t,
+                         std::int64_t _dim_size) -> torch::Tensor {
+      auto out = torch::zeros({_dim_size, _msg.size(-1)});
+
+      // Number of messages is t.numel();
+      if (_t.numel() > 0) {
+        const auto argmax = scatter_argmax(_t, _index, _dim_size);
+        const auto mask =
+            argmax < _msg.size(0);  // Items with at least one entry
+        const auto latest_msgs = _msg.index_select(0, argmax.index({mask}));
+        out.index_put_({mask}, latest_msgs);
+      }
+
+      return out;
+    };
+
     const auto aggr =
         last_aggr(msg, assoc_.index_select(0, idx), t, n_id.size(0));
 
@@ -237,57 +286,19 @@ struct TGNMemoryImpl : torch::nn::Module {
     return {updated_memory, updated_last_update};
   }
 
-  auto update_msg_store(const torch::Tensor& src, const torch::Tensor& dst,
-                        const torch::Tensor& t, const torch::Tensor& raw_msg,
-                        bool is_src_store) -> void {
-    // Group interactions by node ID
-    const auto [n_id_sorted, perm] = src.sort();
-    const auto [unique_nid, _, count] = torch::unique_consecutive(
-        n_id_sorted, /*return_inverse=*/true, /*return_counts=*/true);
-
-    // Convert count tensor to a C++ vector for split_with_sizes
-    auto* count_data_ptr = count.data_ptr<std::int64_t>();
-    std::vector<std::int64_t> sizes(count_data_ptr,
-                                    count_data_ptr + count.numel());
-
-    // Reorder all data based on the sorted node IDs and split them
-    const auto src_s = src.index_select(0, perm).split_with_sizes(sizes);
-    const auto dst_s = dst.index_select(0, perm).split_with_sizes(sizes);
-    const auto t_s = t.index_select(0, perm).split_with_sizes(sizes);
-    const auto msg_s = raw_msg.index_select(0, perm).split_with_sizes(sizes);
-
-    auto& store = is_src_store ? src_store_ : dst_store_;
-
-    for (std::int64_t i = 0; i < unique_nid.size(0); ++i) {
-      const auto key = unique_nid[i].item<std::int64_t>();
-      const auto value = std::make_tuple(src_s[i], dst_s[i], t_s[i], msg_s[i]);
-      store[key] = value;
-    }
-  }
-
   auto compute_msg(const torch::Tensor& n_id, bool is_src_store)
       -> std::tuple<torch::Tensor, torch::Tensor, torch::Tensor> {
-    // Gather stored messages
-    std::vector<torch::Tensor> src_list;
-    std::vector<torch::Tensor> dst_list;
-    std::vector<torch::Tensor> t_list;
-    std::vector<torch::Tensor> raw_msg_list;
-
     const auto& store = is_src_store ? src_store_ : dst_store_;
-    for (std::int64_t i = 0; i < n_id.numel(); ++i) {
-      auto node = n_id[i].item<std::int64_t>();
-      const auto& data = store[node];
 
-      src_list.push_back(std::get<0>(data));
-      dst_list.push_back(std::get<1>(data));
-      t_list.push_back(std::get<2>(data));
-      raw_msg_list.push_back(std::get<3>(data));
-    }
+    // Find which nodes in the current batch have messages in oru store
+    const auto mask = store.time_.index_select(0, n_id) > 0;
+    const auto active_n_id = n_id.index({mask});
 
-    const auto src = torch::cat(src_list, 0);
-    const auto dst = torch::cat(dst_list, 0);
-    const auto t = torch::cat(t_list, 0);
-    const auto raw_msg = torch::cat(raw_msg_list, 0);
+    // Gather message
+    const auto src = store.src_.index_select(0, active_n_id);
+    const auto dst = store.dst_.index_select(0, active_n_id);
+    const auto t = store.time_.index_select(0, active_n_id);
+    const auto raw_msg = store.msg_.index_select(0, active_n_id);
 
     // Compute msg components
     const auto rel_t = t - last_update_.index_select(0, src);
@@ -301,22 +312,6 @@ struct TGNMemoryImpl : torch::nn::Module {
     return std::make_tuple(msg, t, src);
   }
 
-  static auto last_aggr(const torch::Tensor& msg, const torch::Tensor& index,
-                        const torch::Tensor& t, std::int64_t dim_size)
-      -> torch::Tensor {
-    auto out = torch::zeros({dim_size, msg.size(-1)});
-
-    // Number of messages is t.numel();
-    if (t.numel() > 0) {
-      const auto argmax = scatter_argmax(t, index, dim_size);
-      const auto mask = argmax < msg.size(0);  // Items with at least one entry
-      const auto latest_msgs = msg.index_select(0, argmax.index({mask}));
-      out.index_put_({mask}, latest_msgs);
-    }
-
-    return out;
-  }
-
   std::size_t msg_dim_{};
   std::size_t num_nodes_{};
   torch::Tensor memory_;
@@ -326,9 +321,7 @@ struct TGNMemoryImpl : torch::nn::Module {
   TimeEncoder time_encoder_{nullptr};
   torch::nn::GRUCell gru_{nullptr};
 
-  std::vector<
-      std::tuple<torch::Tensor, torch::Tensor, torch::Tensor, torch::Tensor>>
-      src_store_, dst_store_;
+  MsgStore src_store_, dst_store_;
 };
 TORCH_MODULE(TGNMemory);
 }  // namespace detail
